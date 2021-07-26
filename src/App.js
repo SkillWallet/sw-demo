b828873e14d02a771e49b83018c8d95a25cb1c45
Milena Monova <mimonova13@gmail.com>
2021-07-26 03:08:11 +0300

    skillwallet package version updated

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <skillwallet-auth partnerId='123'></skillwallet-auth>
+        <skillwallet-auth communityAddress=''></skillwallet-auth>
       </header>
     </div>
   );
