4110cfe05251c7ddc5905af6673bda5cb17de1fe
Milena Monova <mimonova13@gmail.com>
2021-07-28 15:16:11 +0300

    SW version bumped

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <skillwallet-auth communityAddress=''></skillwallet-auth>
+        {/* <skillwallet-auth partner-key="d0aa09caba3ee6e60eb4b2724e9909df5328c599"></skillwallet-auth> */}
+        {/* <skillwallet-auth partner-key="b23ab97b164c9dd850d02380c1a20f3f659d0c4d"></skillwallet-auth> */}
+        <skillwallet-auth partner-key="6a918cdf9ae3d32131c779c22ae30290a2e729c3"></skillwallet-auth>
       </header>
     </div>
   );
