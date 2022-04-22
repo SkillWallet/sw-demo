5e62e7701643d4dcb923e70ae7f17d78cb77e787
migrenaa <mimonova13@gmail.com>
2022-04-22 20:25:43 +0200

    demo

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import "./App.css";
-import { InitSwAuth } from "@skill-wallet/auth";
-import { useEffect } from "react";
+import './App.css';
+import { InitSwAuth } from '@skill-wallet/auth';
+import {useEffect} from 'react';
+
 function App() {
+
   useEffect(() => {
-    InitSwAuth();
+      InitSwAuth();
   }, []);
 
   return (
     <div>
-      <sw-auth partner-key="877e55ee9cdcb52f0c170258eafaf64581eab9b3" use-dev='true'></sw-auth>
+        <sw-auth partner-key="0772ffab1977b915c1a8bc8ea073c8bdbdecd71f" use-dev="true"></sw-auth>
     </div>
   );
 }
+
 export default App;
